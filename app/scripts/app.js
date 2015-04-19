c3080731e456fc33c371f344697aab5581180ee3
Develra <michael.drew.aaron@gmail.com>
2015-04-18 18:36:31 -0600

    Initial styles

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -25,27 +25,21 @@ var Output = React.createClass({
     render: function() {
         return (
             <div id="output">
-                <div className="row" id="solution">
-                    <div className="col-md-4"></div>
-                    <div className="col-md-4">
-                        <h5 id="expected-output-title">Expected Output</h5>
-                        <input className="form-control" id="solution-output"></input>
+                <div className="row">
+                    <div className="col-md-6 col-md-offset-5">
+                        <input className="form-control" id="solution-output" placeholder="Expected Output"></input>
                     </div>
                 </div>
-                <div className="row" id="users">
-                    <div className="col-md-2"></div>
-                    <div className="col-md-3" id="user">
-                        <h5> Your Output </h5>
-                        <input className="form-control" id="user-output"></input>
+                <div className="row">
+                    <div className="col-md-3 col-md-offset-5">
+                        <input className="form-control" id="user-output" placeholder="Your Output"></input>
                     </div>
-                    <div className="col-md-2">
-                      <CountdownTimer secondsRemaining="15" />
+                    <div className="col-md-3 col-mod-offset-6" id="opponent">
+                        <input className="form-control" id="opponent-output" placeholder="Opponent Output"></input>
                     </div>
-                    <div className="col-md-3" id="opponent">
-                        <h5> Opponents Output </h5>
-                        <input className="form-control" id="opponent-output"></input>
-                    </div>
-                    <div className="col-md-2"></div>
+                </div>
+                <div className="row">
+                    <CountdownTimer secondsRemaining="15" />
                 </div>
             </div>
         );
@@ -58,20 +52,19 @@ var Boilerplate = React.createClass({
             <div id="boilerplate">
                 <p> {this.props.boilerplate} </p>
             </div>
-        )
+        );
     }
 
 });
 
-var UserCode = React.createClass({
+var CodeBox = React.createClass({
     handleClick: function() {
         this.props.clicked();
     },
     render: function() {
         return (
-            <div id="user-code">
-                <h3>Write some code to solve the problem</h3>
-                <textarea className="form-control" id="code" rows="3"></textarea>
+            <div id="code-box">
+                <textarea className="form-control" id="code" rows="3" placeholder="Write some code to solve the question"></textarea>
                 <button className="btn btn-default" id="submit" onClick={this.handleClick}>Submit</button>
             </div>
         )
@@ -117,21 +110,14 @@ var OneLinerApp = React.createClass({
 
 
   render: function() {
-    var boilerplate = "function(input){\n\treturn USER-CODE\n}";
     var q = {question: 'Return the input array without any sevens', testCases: [{input: [1,2,7,4,5,6], output: [1,2,4,5,6]}]}
     return (
-      <div>
-        <h1>OneLiner</h1>
+      <div className="container-fluid">
         <Question question={q.question} />
-        <div className="row">
-            <div className="col-md-3">
-                <Boilerplate boilerplate={boilerplate} />
-            </div>
             <div className="col-md-9">
                 <Output expectedSolution={q.testCases[0].output} />
             </div>
-        </div>
-        <UserCode clicked={this.onSubmit}/>
+        <CodeBox clicked={this.onSubmit}/>
       </div>
     );
   }
