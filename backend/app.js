03506d67ef62e0d52db64948d0e2dfc620e98e36
Peyman Mortazavi <pmortazavi@cailmobility.com>
2015-04-19 18:19:22 -0600

    added code test logic (backend)

--- backend/app.js
+++ backend/app.js
@@ -40,16 +40,16 @@ io.on('connection', function(socket) {
     var firstPlayerSocket = playerQueue.shift();
 
     // Create a session object.
-    var sessionObject = {id: shortid.generate(), player1: firstPlayerSocket.id, player2: socket.id, question: getRandomQuestion()};
-    gameSessions.push();
+    var sessionObject = {id: shortid.generate(), player1: firstPlayerSocket, player2: socket, question: getRandomQuestion()};
+    gameSessions.push(sessionObject);
 
     // Remove "test_cases" from question object so front-end won't see test-cases.
     var questionObject = JSON.parse(JSON.stringify(sessionObject.question));
     delete questionObject.test_cases;
 
     // Inform players in the session.
-    firstPlayerSocket.emit("match_made", { session_id: sessionObject.id, opponent_id: sessionObject.player2, question: questionObject });
-    socket.emit("match_made", { session_id: sessionObject.id, opponent_id: sessionObject.player1, question: questionObject });
+    firstPlayerSocket.emit("match_made", { session_id: sessionObject.id, opponent_id: sessionObject.player2.id, question: questionObject });
+    socket.emit("match_made", { session_id: sessionObject.id, opponent_id: sessionObject.player1.id, question: questionObject });
 
   } else {
 
@@ -60,11 +60,37 @@ io.on('connection', function(socket) {
 
   socket.on("submit_answer", function(packet) {
 
-    // if client doesn't provide packet, ignore the request.
-    if(!packet.session_id)
+    console.log("Received an answer: " + JSON.stringify(packet));
+
+    // if client doesn't provide required fields, ignore the package.
+    if(!packet.session_id || !packet.code)
       return;
 
+    var gameSession = _.find(gameSessions, function(item){ return item.id == packet.session_id; });
+    if(!gameSession)
+        return;
 
+    // Test the code.
+    // TODO: add some logic to remove potential malicious code.
+    var fn = new Function('input', 'return ' + packet.code);
+    for(var i=0; i < gameSession.question.test_cases.length; i++) {
+
+        var test_case = gameSession.question.test_cases[i];
+
+        var actualValue = fn(test_case.input);
+        if(actualValue != test_case.output) {
+            console.log(test_case);
+            socket.emit("incorrect_answer", {"expected_value": test_case.output, "input_value": test_case.input, "actual_value": actualValue});
+            return;
+        }
+    };
+
+    console.log("we have a winner");
+    // We have a winner!
+    var gameOverObject = {winner: socket.id, code: packet.code};
+    socket.emit("game_over", gameOverObject);
+    gameSession.player2.emit("game_over", gameOverObject);
+    _.remove(gameSessions, gameSession);
 
   });
 
