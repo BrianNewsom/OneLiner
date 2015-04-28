23e04d9f1a4dd9ef0041a0a7328dc8c033b199e4
Develra <michael.drew.aaron@gmail.com>
2015-04-28 17:04:44 -0600

    heroku fix - attempt 3

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var port = process.env.PORT || 3000;
 app.use(express.static('dist'));
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + 'index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 function getRandomQuestion() {
